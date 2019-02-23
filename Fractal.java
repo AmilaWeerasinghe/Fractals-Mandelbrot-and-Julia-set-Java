7412b4f7d29461e570ad33ac9f8de76deaf6b86b
Amila Weerasinghe <40320449+AmilaWeerasinghe@users.noreply.github.com>
2019-02-23 21:30:50 +0530

    Update Fractal.java
    Totally working Fractal for Mandelbrot
    Option 1- Mandelbrot
    Option 2- Mandelbrot with 4 arguments(Coordinates)
    Option 3-Mandelbrot with 5 arguments(Coordinates+Number of iterations)

--- Fractal.java
+++ Fractal.java
@@ -16,7 +16,10 @@ public class Fractal {
     String[] splitted=input.split(" ");
    if(splitted[0].equals("Mandelbrot")){
 	   System.out.println("Mandelbrot Set selected");
-	   
+	   if(splitted.length==1){
+		   
+		   
+		   //crete threds and end and start of cols and rows are the inputs
 	   Thread thread1 = new Thread(new MandelbrotThread(0,400,0,400));
        Thread thread2 = new Thread(new MandelbrotThread(0,400,400,800));
        Thread thread3 = new Thread(new MandelbrotThread(400,800,0,400));
@@ -54,23 +57,117 @@ public class Fractal {
 	   (Frame.frame).pack();
 	   (Frame.frame).setVisible(true);
 	   
-	   
-	// create a frame for Mandelbrot Set
-		/*JFrame frame = new JFrame("Fractals Mandelbrot Set"); 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-
-		// set the content of the frame as one of this panel
-		frame.setContentPane(new Panel(800, 800)); 
-
-		frame.pack(); 
-		frame.setLocationRelativeTo(null); 
-		frame.setVisible(true); */
-		
-		//default mandelbrot set zero arguments given
-		if(splitted.length==1){
-			System.out.println("Default MandelBrot is ON");
+	   }
+	
+		if(splitted.length==5){
+			System.out.println("Mandelbrot with 4 arguments");
+			
+              //take input variables
+            float xpositive = Float.parseFloat(splitted[1]);
+            float xnegative = Float.parseFloat(splitted[2]);
+            float ypositive = Float.parseFloat(splitted[3]);
+            float ynegative = Float.parseFloat(splitted[4]);
+            
+            //System.out.println("xp "+xpositive+"yN"+ynegative);
+            
+           //create threads
+            Thread thread1 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,0,400,0,400));
+            Thread thread2 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,0,400,400,800));
+            Thread thread3 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,400,800,0,400));
+            Thread thread4 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,400,800,400,800));
+            //start threads
+            thread1.start();thread2.start();thread3.start(); thread4.start();
+            //join threads
+            try {
+        		thread1.join();thread2.join();thread3.join();thread4.join();
+        	} catch (InterruptedException e) {
+        		// TODO Auto-generated catch block
+        		e.printStackTrace();
+        	}
+            
+          //create object of Frame class to draw Mandelbrot
+     	   Frame frame=new Frame("This is Mandelbrot Set");
+     	   
+     	   //go through every point n the Frame
+     	   for(int row=0;row<800;row++){
+     		   for(int col=0;col<800;col++){
+     			//check out each point which now assigned with int called itearations and depnd on that assign colors
+     			  if(MandelbrotThread.array[row][col]< MandelbrotThread.maxiterations){
+     				  //pixel in mandelbrot(it is good create Colour class)
+     				  Frame.image.setRGB(col, row, Colour.setColor(MandelbrotThread.array[row][col], MandelbrotThread.maxiterations));
+     				  
+     			  } 
+     			  else{
+     				  //now not in Mandelbrot assign black
+     				  (Frame.image).setRGB(col, row,0);
+     			  } 
+     		   }
+     	   }
+     	   
+     	   //complete the frame
+     	   (Frame.frame).add(new JLabel(new ImageIcon(Frame.image)));
+     	   (Frame.frame).pack();
+     	   (Frame.frame).setVisible(true);
+            
 		}
+		
+		//for 5 arguments (with number of iterations)
 	   
+		if(splitted.length==6){
+			System.out.println("Mandelbrot with 5 arguments");
+			
+              //take input variables
+            float xpositive = Float.parseFloat(splitted[1]);
+            float xnegative = Float.parseFloat(splitted[2]);
+            float ypositive = Float.parseFloat(splitted[3]);
+            float ynegative = Float.parseFloat(splitted[4]);
+            int iterations=Integer.parseInt(splitted[5]);
+            //System.out.println("iteratins "+iterations);
+            
+           //create threads Constructor with threads is used
+            Thread thread1 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,0,400,0,400,iterations));
+            Thread thread2 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,0,400,400,800,iterations));
+            Thread thread3 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,400,800,0,400,iterations));
+            Thread thread4 = new Thread(new MandelbrotThread(xpositive,xnegative,ypositive,ynegative,400,800,400,800,iterations));
+            //start threads
+            thread1.start();thread2.start();thread3.start(); thread4.start();
+            //join threads
+            try {
+        		thread1.join();thread2.join();thread3.join();thread4.join();
+        	} catch (InterruptedException e) {
+        		// TODO Auto-generated catch block
+        		e.printStackTrace();
+        	}
+            
+          //create object of Frame class to draw Mandelbrot
+     	   Frame frame=new Frame("This is Mandelbrot Set");
+     	   
+     	   //go through every point n the Frame
+     	   for(int row=0;row<800;row++){
+     		   for(int col=0;col<800;col++){
+     			//check out each point which now assigned with int called itearations and depnd on that assign colors
+     			  if(MandelbrotThread.array[row][col]< MandelbrotThread.maxiterations){
+     				  //pixel in mandelbrot(it is good create Colour class)
+     				  Frame.image.setRGB(col, row, Colour.setColor(MandelbrotThread.array[row][col], MandelbrotThread.maxiterations));
+     				  
+     			  } 
+     			  else{
+     				  //now not in Mandelbrot assign black
+     				  (Frame.image).setRGB(col, row,0);
+     			  } 
+     		   }
+     	   }
+     	   
+     	   //complete the frame
+     	   (Frame.frame).add(new JLabel(new ImageIcon(Frame.image)));
+     	   (Frame.frame).pack();
+     	   (Frame.frame).setVisible(true);
+            
+		}
+		
+		
+		
+		//
    }
    if(splitted[0].equals("Julia")){
 	   System.out.println("Julia set selected");
