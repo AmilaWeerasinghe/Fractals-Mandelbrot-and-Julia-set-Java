d2bca5f6a0ecf5d77e81bec273df80eec5c9d60b
Amila Weerasinghe <40320449+AmilaWeerasinghe@users.noreply.github.com>
2019-02-23 20:53:33 +0530

    Update Fractal.java
    Fully working code when user input "Mandelbrot"
    i.e Default Sandelbrot set

--- Fractal.java
+++ Fractal.java
@@ -1,7 +1,9 @@
 package Fractals;
 import java.util.*;
 
-import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+//import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Fractal {
 
@@ -10,18 +12,51 @@ public class Fractal {
 		Scanner keyboard =new Scanner(System.in);
 	System.out.println("Please Enter The Details: ");
 	String input=keyboard.nextLine();
-	//System.out.println("the input is "+input);
-	//breaking line of input
-    String[] splitted=input.split(" ");
-	//int length=splitted.length;
-	//System.out.println("the length is "+length);
 	
-  // System.out.println("the first world is "+splitted[0]);
-   
+    String[] splitted=input.split(" ");
    if(splitted[0].equals("Mandelbrot")){
 	   System.out.println("Mandelbrot Set selected");
+	   
+	   Thread thread1 = new Thread(new MandelbrotThread(0,400,0,400));
+       Thread thread2 = new Thread(new MandelbrotThread(0,400,400,800));
+       Thread thread3 = new Thread(new MandelbrotThread(400,800,0,400));
+       Thread thread4 = new Thread(new MandelbrotThread(400,800,400,800));
+
+       thread1.start();thread2.start();thread3.start(); thread4.start();
+       try {
+		thread1.join();thread2.join();thread3.join();thread4.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   
+	   //create object of Frame class to draw Mandelbrot
+	   Frame frame=new Frame("This is Mandelbrot Set");
+	   
+	   //go through every point n the Frame
+	   for(int row=0;row<800;row++){
+		   for(int col=0;col<800;col++){
+			//check out each point which now assigned with int called itearations and depnd on that assign colors
+			  if(MandelbrotThread.array[row][col]< MandelbrotThread.maxiterations){
+				  //pixel in mandelbrot(it is good create Colour class)
+				  Frame.image.setRGB(col, row, Colour.setColor(MandelbrotThread.array[row][col], MandelbrotThread.maxiterations));
+				  
+			  } 
+			  else{
+				  //now not in Mandelbrot assign black
+				  (Frame.image).setRGB(col, row,0);
+			  } 
+		   }
+	   }
+	   
+	   //complete the frame
+	   (Frame.frame).add(new JLabel(new ImageIcon(Frame.image)));
+	   (Frame.frame).pack();
+	   (Frame.frame).setVisible(true);
+	   
+	   
 	// create a frame for Mandelbrot Set
-		JFrame frame = new JFrame("Fractals Mandelbrot Set"); 
+		/*JFrame frame = new JFrame("Fractals Mandelbrot Set"); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 
 		// set the content of the frame as one of this panel
@@ -29,7 +64,7 @@ public class Fractal {
 
 		frame.pack(); 
 		frame.setLocationRelativeTo(null); 
-		frame.setVisible(true); 
+		frame.setVisible(true); */
 		
 		//default mandelbrot set zero arguments given
 		if(splitted.length==1){
@@ -40,15 +75,7 @@ public class Fractal {
    if(splitted[0].equals("Julia")){
 	   System.out.println("Julia set selected");
 	// create a frame for Julia Set
-			JFrame frame = new JFrame("Fractals Julia Set"); 
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-
-			// set the content of the frame as one of this panel
-			frame.setContentPane(new Panel(800, 800)); 
-
-			frame.pack(); 
-			frame.setLocationRelativeTo(null); 
-			frame.setVisible(true); 
+			
    }
 keyboard.close();
 
