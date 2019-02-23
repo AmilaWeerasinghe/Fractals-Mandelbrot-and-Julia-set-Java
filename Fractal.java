20954a204f7cc73fef0e4eb732bd7a1987a48900
Amila Weerasinghe <40320449+AmilaWeerasinghe@users.noreply.github.com>
2019-02-24 00:01:22 +0530

    Update Fractal.java
    100% woking code
    3 options for Mandelbrot set
    3 options for Julia set included

--- Fractal.java
+++ Fractal.java
@@ -19,7 +19,7 @@ public class Fractal {
 	   if(splitted.length==1){
 		   
 		   
-		   //crete threds and end and start of cols and rows are the inputs
+		   //create threads and end and start of cols and rows are the inputs
 	   Thread thread1 = new Thread(new MandelbrotThread(0,400,0,400));
        Thread thread2 = new Thread(new MandelbrotThread(0,400,400,800));
        Thread thread3 = new Thread(new MandelbrotThread(400,800,0,400));
@@ -172,12 +172,159 @@ public class Fractal {
    if(splitted[0].equals("Julia")){
 	   System.out.println("Julia set selected");
 	// create a frame for Julia Set
-			
+	   //now the values for Zn and C have changed
+			//create a new class
+	   if(splitted.length==1){
+		  //only julia word default Julia set
+		   System.out.println("Default Juliaset");
+		   
+		   //create threads and end and start of cols and rows are the inputs
+		   Thread thread1 = new Thread(new JuliaThread(0,400,0,400));
+	       Thread thread2 = new Thread(new JuliaThread(0,400,400,800));
+	       Thread thread3 = new Thread(new JuliaThread(400,800,0,400));
+	       Thread thread4 = new Thread(new JuliaThread(400,800,400,800));
+
+	       thread1.start();thread2.start();thread3.start(); thread4.start();
+	       try {
+			thread1.join();thread2.join();thread3.join();thread4.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   
+		   //create object of Frame class to draw Mandelbrot
+		   Frame frame=new Frame("This is Mandelbrot Set");
+		   
+		   //go through every point n the Frame
+		   for(int row=0;row<800;row++){
+			   for(int col=0;col<800;col++){
+				//check out each point which now assigned with int called itearations and depnd on that assign colors
+				  if(JuliaThread.array[row][col]< JuliaThread.maxiterations){
+					  //pixel in mandelbrot(it is good create Colour class)
+					  Frame.image.setRGB(col, row, Colour.setColor(JuliaThread.array[row][col], JuliaThread.maxiterations));
+					  
+				  } 
+				  else{
+					  //now not in Mandelbrot assign black
+					  (Frame.image).setRGB(col, row,0);
+				  } 
+			   }
+		   }
+		   
+		   //complete the frame
+		   (Frame.frame).add(new JLabel(new ImageIcon(Frame.image)));
+		   (Frame.frame).pack();
+		   (Frame.frame).setVisible(true);
+		   
+		   }
+	   if(splitted.length==4){
+		   //C and iterations given by the user
+		 //only julia word default Julia set
+		   System.out.println("Juliaset C and iteations given");
+		   //take C and iteartions from input
+		   
+		   Double real = Double.parseDouble(splitted[1]);
+		   Double img = Double.parseDouble(splitted[2]);
+		   int itr =Integer.parseInt(splitted[3]);
+		   
+		   
+		   //create threads and end and start of cols and rows are the inputs constructor changed
+		   Thread thread1 = new Thread(new JuliaThread(0,400,0,400,real,img,itr));
+	       Thread thread2 = new Thread(new JuliaThread(0,400,400,800,real,img,itr));
+	       Thread thread3 = new Thread(new JuliaThread(400,800,0,400,real,img,itr));
+	       Thread thread4 = new Thread(new JuliaThread(400,800,400,800,real,img,itr));
+
+	       thread1.start();thread2.start();thread3.start(); thread4.start();
+	       try {
+			thread1.join();thread2.join();thread3.join();thread4.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   
+		   //create object of Frame class to draw Mandelbrot
+		   Frame frame=new Frame("This is Julia Set");
+		   
+		   //go through every point n the Frame
+		   for(int row=0;row<800;row++){
+			   for(int col=0;col<800;col++){
+				//check out each point which now assigned with int called itearations and depnd on that assign colors
+				  if(JuliaThread.array[row][col]< JuliaThread.maxiterations){
+					  //pixel in mandelbrot(it is good create Colour class)
+					  Frame.image.setRGB(col, row, Colour.setColor(JuliaThread.array[row][col], JuliaThread.maxiterations));
+					  
+				  } 
+				  else{
+					  //now not in Mandelbrot assign black
+					  (Frame.image).setRGB(col, row,0);
+				  } 
+			   }
+		   }
+		   
+		   //complete the frame
+		   (Frame.frame).add(new JLabel(new ImageIcon(Frame.image)));
+		   (Frame.frame).pack();
+		   (Frame.frame).setVisible(true);
+		   
+	   }
+	   if(splitted.length==3){
+		   //C and iterations given by the user
+		 //only julia word default Julia set
+		   System.out.println("Juliaset C given");
+		   //take C and iteartions from input
+		   
+		   Double real = Double.parseDouble(splitted[1]);
+		   Double img = Double.parseDouble(splitted[2]);
+		   //int itr =Integer.parseInt(splitted[3]);
+		   
+		   
+		   //create threads and end and start of cols and rows are the inputs constructor changed
+		   Thread thread1 = new Thread(new JuliaThread(0,400,0,400,real,img));
+	       Thread thread2 = new Thread(new JuliaThread(0,400,400,800,real,img));
+	       Thread thread3 = new Thread(new JuliaThread(400,800,0,400,real,img));
+	       Thread thread4 = new Thread(new JuliaThread(400,800,400,800,real,img));
+
+	       thread1.start();thread2.start();thread3.start(); thread4.start();
+	       try {
+			thread1.join();thread2.join();thread3.join();thread4.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   
+		   //create object of Frame class to draw Mandelbrot
+		   Frame frame=new Frame("This is Julia Set");
+		   
+		   //go through every point n the Frame
+		   for(int row=0;row<800;row++){
+			   for(int col=0;col<800;col++){
+				//check out each point which now assigned with int called itearations and depnd on that assign colors
+				  if(JuliaThread.array[row][col]< JuliaThread.maxiterations){
+					  //pixel in mandelbrot(it is good create Colour class)
+					  Frame.image.setRGB(col, row, Colour.setColor(JuliaThread.array[row][col], JuliaThread.maxiterations));
+					  
+				  } 
+				  else{
+					  //now not in Mandelbrot assign black
+					  (Frame.image).setRGB(col, row,0);
+				  } 
+			   }
+		   }
+		   
+		   //complete the frame
+		   (Frame.frame).add(new JLabel(new ImageIcon(Frame.image)));
+		   (Frame.frame).pack();
+		   (Frame.frame).setVisible(true);
+		   
+	   }   
+		   
+	   }
+   keyboard.close();   
    }
-keyboard.close();
+
 
 
 
 	}
 
-}
+//}
